dd575a78d46e0c120c2cf7b857b61925afc4f4cd
cuingskot76 <afrizapamujo@gmail.com>
2023-02-26 14:52:23 +0700

    tambahan tugas 2

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -31,26 +31,35 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(670, 76);
+            this.button1.Location = new System.Drawing.Point(244, 65);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Pilih Image";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(670, 154);
+            this.button2.Location = new System.Drawing.Point(244, 117);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(117, 54);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Intensitas (0,0) dan (n-1, m-1)";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -58,22 +67,90 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(44, 40);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(434, 304);
+            this.pictureBox1.Size = new System.Drawing.Size(179, 268);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(244, 203);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Grayscale";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(382, 40);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(257, 268);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(657, 65);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 42);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Peningkatan kecerahan";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(657, 148);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(108, 45);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Membalik citra";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(802, 40);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(174, 268);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(382, 344);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(257, 250);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(988, 629);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -83,5 +160,11 @@
         private Button button1;
         private Button button2;
         private PictureBox pictureBox1;
+        private Button button3;
+        private PictureBox pictureBox2;
+        private Button button4;
+        private Button button5;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -57,5 +57,114 @@ namespace WinFormsApp1
             Debug.WriteLine("(" + (n - 1) + "," + (m - 1) + ") : B=" + b);
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // get image from "image form 1"
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+
+            int r, g, b;
+            int i;
+
+            // get image specifications
+            int m = bmp.Height;
+            int n = bmp.Width;
+
+            Color p;
+
+            #region 
+            for (int y = 0; y < m - 1; y++)
+            {
+                for (int x = 0; x < n - 1; x++)
+                {
+                    // get pixel from (x, y)
+                    p = bmp.GetPixel(x, y);
+                    r = p.R;
+                    g = p.G;
+                    b = p.B;
+
+                    i = Convert.ToInt16(0.2989 * r + 0.5870 * g + 0.1141 * b);
+
+                    // convert pixel values(r, g, b) with i
+                    bmp.SetPixel(x, y, Color.FromArgb(i, i, i));
+                }
+            }
+            // show the result to "image form 2"
+            pictureBox2.Image = bmp;
+            #endregion
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox2.Image);
+
+            int r, g, b;
+            int i;
+
+            int m = bmp.Height;
+            int n = bmp.Width;
+
+            Color p;
+
+            for (int y = 0; y < m - 1; y++)
+            {
+                for (int x = 0; x < n - 1; x++)
+                {
+                    p = bmp.GetPixel(x, y);
+                    r = p.R;
+
+                    // intensitas ditambah B = 40
+                    i = r + 40;
+
+                    if (i > 255)
+                    {
+                        i = 255;
+                    }
+                    else if (i < 0)
+                    {
+                        i = 0;
+                    }
+
+                    bmp.SetPixel(x, y, Color.FromArgb(i, i, i));
+                }
+            }
+            pictureBox3.Image = bmp;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox2.Image);
+
+            int m = bmp.Height;
+            int n = bmp.Width;
+
+            for (int y = 0; y < m - 1; y++)
+            {
+                for (int x = 0; x < n - 1; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+
+                    p = Color.FromArgb(255, (255 - p.R), (255 - p.G), (255 - p.B));
+                    bmp.SetPixel(x, y, p);
+
+                    // int xg = (int)((p.R + p.G + p.B) / 3);
+                    // int xi = 255 - xg;
+                    // 
+                    // Color new_color = Color.FromArgb(xi, xi, xi);
+                    // bmp.SetPixel(x, y, new_color);
+                }
+            }
+            pictureBox4.Image = bmp;
+        }
     }
 }
